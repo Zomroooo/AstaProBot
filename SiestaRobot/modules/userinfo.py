eb09944f31b7f7bbca3e96c162e05ac32f979bc4
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-05 01:01:36 +0530

    Update userinfo.py

--- SiestaRobot/modules/userinfo.py
+++ SiestaRobot/modules/userinfo.py
@@ -544,6 +544,43 @@ def __user_info__(user_id):
 def helps(chat):
     return gs(chat, "info_and_afk_help")
 
+
+__help__ = """
+  ──「 ZOMBIES 」──
+  
+*ID:*
+× /id*:* get the current group id. If used by replying to a message, gets that user's id.
+× /gifid*:* reply to a gif to me to tell you its file ID.
+ 
+*Self addded information:* 
+× /setme <text>*:* will set your info
+× /me*:* will get your or another user's info.
+Examples:
+× /setme I am a wolf.
+× /me @username(defaults to yours if no user specified)
+ 
+*Information others add on you:* 
+× /bio*:* will get your or another user's bio. This cannot be set by yourself.
+× /setbio <text>*:* while replying, will save another user's bio 
+Examples:
+× /bio @username(defaults to yours if not specified).
+× /setbio This user is a wolf (reply to the user)
+ 
+*Overall Information about you:*
+× /info*:* get information about a user. 
+ 
+*json Detailed info:*
+× /json*:* Get Detailed info about any message.
+ 
+*ARQ Statistics:*
+× /arq*:* ARQ API STATS
+*Afk:*
+× /afk*:* away from keyboard
+×  brb*:* ded in the chat
+*What is that health thingy?*
+ Come and see [HP System explained](https://t.me/TeamNexusX)
+"""
+
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio, run_async=True)
 GET_BIO_HANDLER = DisableAbleCommandHandler("bio", about_bio, run_async=True)
 
