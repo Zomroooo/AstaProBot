a663d64aa54470c55c7860b3938fca49c2f6be73
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-05 03:40:37 +0530

    Update forcesubs.py

--- SiestaRobot/modules/forcesubs.py
+++ SiestaRobot/modules/forcesubs.py
@@ -180,4 +180,23 @@ def config(client, message):
 def helps(chat):
     return gs(chat, "fsub_feds_help")
 
+__help__ = """
+  ──「 FORCESUB 」──
+
+*Force Subscribe:*
+• Hinata can mute members who are not subscribed your channel until they subscribe
+• When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
+*Setup*
+*Only creator*
+• Add me in your group as admin
+• Add me in your channel as admin 
+ 
+*Commmands*
+ • /fsub {channel username} - To turn on and setup the channel.
+   Do this first...
+ • /fsub - To get the current settings.
+ • /fsub disable - To turn of ForceSubscribe..
+   If you disable fsub, you need to set again for working.. /fsub {channel username} 
+ • /fsub clear - To unmute all members who muted by me.
+"""
 __mod_name__ = "F-Sub/Feds"
