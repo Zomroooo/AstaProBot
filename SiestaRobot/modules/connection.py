a3a36feccdd7c1997aa870f5329090c16914f800
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-05 03:49:07 +0530

    Update connection.py

--- SiestaRobot/modules/connection.py
+++ SiestaRobot/modules/connection.py
@@ -403,6 +403,18 @@ def connect_button(update, context):
 def helps(chat):
     return gs(chat, "connection_help")
 
+__help__ = """
+  ──「 CONNECTION 」──
+
+Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
+This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
+ • /connect: Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
+ • /connection: List connected chats
+ • /disconnect: Disconnect from a chat
+ • /helpconnect: List available commands that can be used remotely
+*Admin only:*
+ • /allowconnect <yes/no>: allow a user to connect to a chat
+"""
 __mod_name__ = "Connection"
 
 CONNECT_CHAT_HANDLER = CommandHandler(
