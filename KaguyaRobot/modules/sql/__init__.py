c4ec65eb3b1be2c0a7a7d29536778a8fd244aacb
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 00:00:13 +0530

    Update __init__.py

--- KaguyaRobot/modules/sql/__init__.py
+++ KaguyaRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from SiestaRobot import DB_URL
+from KaguyaRobot import DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
