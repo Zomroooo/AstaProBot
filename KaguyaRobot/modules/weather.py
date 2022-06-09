a226c6af253f4f820a069f24a21240d30fa8131e
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 18:46:28 +0530

    Update weather.py

--- KaguyaRobot/modules/weather.py
+++ KaguyaRobot/modules/weather.py
@@ -8,10 +8,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from SiestaRobot import *
+from KaguyaRobot import *
 
-from SiestaRobot import telethn as tbot
-from SiestaRobot.events import register
+from KaguyaRobot import telethn as tbot
+from KaguyaRobot.events import register
 
 
 async def is_register_admin(chat, user):
