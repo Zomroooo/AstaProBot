1c8104b2b84266e2640b384b9d88a7b69f4cb795
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 18:19:19 +0530

    Update arqapi.py

--- KaguyaRobot/utils/arqapi.py
+++ KaguyaRobot/utils/arqapi.py
@@ -4,15 +4,15 @@ from random import randint
 from time import time
 
 import aiohttp
-from SiestaRobot import aiohttpsession 
+from KaguyaRobot import aiohttpsession 
 from aiohttp import ClientSession
 
 from google_trans_new import google_translator
 from Python_ARQ import ARQ
 from search_engine_parser import GoogleSearch
 
-from SiestaRobot import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY
-from SiestaRobot import pbot
+from KaguyaRobot import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY
+from KaguyaRobot import pbot
 
 ARQ_API = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
 ARQ_API_KEY = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
