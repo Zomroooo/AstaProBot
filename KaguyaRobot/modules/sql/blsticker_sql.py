8db485beb2f0ff36207e42adf632c847787fe825
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 00:54:38 +0530

    Update blsticker_sql.py

--- KaguyaRobot/modules/sql/blsticker_sql.py
+++ KaguyaRobot/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func, Integer
 
-from SiestaRobot.modules.sql import BASE, SESSION
+from KaguyaRobot.modules.sql import BASE, SESSION
 
 
 class StickersFilters(BASE):
