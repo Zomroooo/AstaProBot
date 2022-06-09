0f57791ed4860bcd5fe6d9fb09e30107509dab8f
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 01:07:51 +0530

    Update extraction.py

--- KaguyaRobot/modules/helper_funcs/extraction.py
+++ KaguyaRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from SiestaRobot import LOGGER
-from SiestaRobot.modules.users import get_user_id
+from KaguyaRobot import LOGGER
+from KaguyaRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
