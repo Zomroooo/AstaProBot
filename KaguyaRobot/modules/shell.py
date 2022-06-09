8ac7567df8d72cab6be6be6c00ae87f5a798397b
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 22:57:43 +0530

    Update shell.py

--- KaguyaRobot/modules/shell.py
+++ KaguyaRobot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from SiestaRobot import LOGGER, dispatcher
-from SiestaRobot.modules.helper_funcs.chat_status import dev_plus
+from KaguyaRobot import LOGGER, dispatcher
+from KaguyaRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
