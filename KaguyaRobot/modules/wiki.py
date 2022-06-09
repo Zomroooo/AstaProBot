993d9e8ec2b466b88fd6acb9beffc0f12cc3c59b
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 18:43:18 +0530

    Update wiki.py

--- KaguyaRobot/modules/wiki.py
+++ KaguyaRobot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from SiestaRobot import dispatcher
-from SiestaRobot.modules.disable import DisableAbleCommandHandler
+from KaguyaRobot import dispatcher
+from KaguyaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
