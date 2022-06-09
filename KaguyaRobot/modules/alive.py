8c668496935df752eec645d04858bbb2d50b14fa
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 17:57:01 +0530

    Create alive.py

---
+++ KaguyaRobot/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from SiestaRobot.events import register
+from SiestaRobot import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/cc77cab3a6093eea22662.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Kaguya.** \n\n"
+  TEXT += "💠 **I'm Working Properly** \n\n"
+  TEXT += f"💠 **My Master : [Zoro](https://t.me/Aceladi)** \n\n"
+  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
+  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/KaguyaProBot?start=help"), Button.url("Support", "https://t.me/NexusXSupport")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
