90a1bb0bcb19b1e57240f1c9713bb0bdd87c5482
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 00:58:23 +0530

    Update forceSubscribe_sql.py

--- KaguyaRobot/modules/sql/forceSubscribe_sql.py
+++ KaguyaRobot/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from SiestaRobot.modules.sql import BASE, SESSION
+from KaguyaRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
