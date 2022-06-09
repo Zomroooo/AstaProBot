7732f24370844974c9430a9a46c0ea0941e07411
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 01:03:41 +0530

    Update __init__.py

--- KaguyaRobot/modules/helper_funcs/telethn/__init__.py
+++ KaguyaRobot/modules/helper_funcs/telethn/__init__.py
@@ -1,4 +1,4 @@
-from SiestaRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn
+from KaguyaRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn
 
 IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
 
