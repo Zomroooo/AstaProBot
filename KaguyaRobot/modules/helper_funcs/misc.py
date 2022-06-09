cbecaebb4c0865876bac6d5b7188b475e928ddaf
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 01:09:03 +0530

    Update misc.py

--- KaguyaRobot/modules/helper_funcs/misc.py
+++ KaguyaRobot/modules/helper_funcs/misc.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from SiestaRobot import NO_LOAD
+from KaguyaRobot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
