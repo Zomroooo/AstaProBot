91a644eac7c24c4bcf1a652965c80529671cc5cf
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 23:50:16 +0530

    Update cash.py

--- KaguyaRobot/modules/cash.py
+++ KaguyaRobot/modules/cash.py
@@ -3,7 +3,7 @@ import requests
 from telegram import Bot, Update
 from telegram.ext import CommandHandler, run_async
 
-from SiestaRobot import dispatcher, CASH_API_KEY
+from KaguyaRobot import dispatcher, CASH_API_KEY
 
 
 def convert(bot: Bot, update: Update):
