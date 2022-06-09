19ecfd6a2e73e25557e916e2e3fc417cd55415b6
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 23:28:22 +0530

    Update karma.py

--- KaguyaRobot/modules/karma.py
+++ KaguyaRobot/modules/karma.py
@@ -3,10 +3,10 @@ from pyrogram import filters
 from aiohttp import ClientSession
 from Python_ARQ import ARQ
 
-from SiestaRobot import pbot as app, BOT_ID
-from SiestaRobot.utils.errors import capture_err
-from SiestaRobot.utils.permissions import adminsOnly
-from SiestaRobot.ex_plugins.dbfunctions import (
+from KaguyaRobot import pbot as app, BOT_ID
+from KaguyaRobot.utils.errors import capture_err
+from KaguyaRobot.utils.permissions import adminsOnly
+from KaguyaRobot.ex_plugins.dbfunctions import (
     alpha_to_int,
     get_karma,
     get_karmas,
@@ -16,8 +16,8 @@ from SiestaRobot.ex_plugins.dbfunctions import (
     karma_on,
     update_karma,
 )      
-from SiestaRobot.utils.filter_groups import karma_negative_group, karma_positive_group
-from SiestaRobot import arq
+from KaguyaRobot.utils.filter_groups import karma_negative_group, karma_positive_group
+from KaguyaRobot import arq
 
 regex_upvote = r"^((?i)\+|\+\+|\+1|thx|thanx|thanks|pro|cool|good|👍)$"
 regex_downvote = r"^(\-|\-\-|\-1|👎|noob|weak)$"
