0ee559d3732163829dd16c43474b0b25f65aa4d7
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 23:56:47 +0530

    Update antichannel.py

--- KaguyaRobot/modules/antichannel.py
+++ KaguyaRobot/modules/antichannel.py
@@ -4,12 +4,12 @@ from telegram.ext.filters import Filters
 from telegram import Update, message, ParseMode
 from telegram.ext import CallbackContext
 
-from SiestaRobot.modules.helper_funcs.decorators import siestacmd, siestamsg
-from SiestaRobot.modules.helper_funcs.channel_mode import user_admin, AdminPerms
-from SiestaRobot.modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
-from SiestaRobot.modules.language import gs
+from KaguyaRobot.modules.helper_funcs.decorators import Kaguyacmd, Kaguyamsg
+from KaguyaRobot.modules.helper_funcs.channel_mode import user_admin, AdminPerms
+from KaguyaRobot.modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
+from KaguyaRobot.modules.language import gs
 
-@siestacmd(command="antichannelmode", group=100)
+@Kaguyacmd(command="antichannelmode", group=100)
 @user_admin(AdminPerms.CAN_RESTRICT_MEMBERS)
 def set_antichannel(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -29,7 +29,7 @@ def set_antichannel(update: Update, context: CallbackContext):
     message.reply_html(
         text=gs(chat.id, "status_antichannel").format(antichannel_status(chat.id), html.escape(chat.title)))
 
-@siestamsg(Filters.chat_type.groups, group=110)
+@Kaguyamsg(Filters.chat_type.groups, group=110)
 def eliminate_channel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
