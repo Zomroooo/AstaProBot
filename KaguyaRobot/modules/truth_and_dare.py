561c0ccfd9656b32dd70fcd1b77245c67e967bd3
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 18:53:50 +0530

    Update truth_and_dare.py

--- KaguyaRobot/modules/truth_and_dare.py
+++ KaguyaRobot/modules/truth_and_dare.py
@@ -1,9 +1,9 @@
 import html
 import random
-import SiestaRobot.modules.truth_and_dare_string as truth_and_dare_string
-from SiestaRobot import dispatcher
+import KaguyaRobot.modules.truth_and_dare_string as truth_and_dare_string
+from KaguyaRobot import dispatcher
 from telegram import ParseMode, Update, Bot
-from SiestaRobot.modules.disable import DisableAbleCommandHandler
+from KaguyaRobot.modules.disable import DisableAbleCommandHandler
 from telegram.ext import CallbackContext, run_async
 
 
