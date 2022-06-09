67dca13eaab49e20e8aed4b111277deaa3a2c62d
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 23:44:01 +0530

    Update dev.py

--- KaguyaRobot/modules/dev.py
+++ KaguyaRobot/modules/dev.py
@@ -5,10 +5,10 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import SiestaRobot
+import KaguyaRobot
 
-from SiestaRobot import dispatcher
-from SiestaRobot.modules.helper_funcs.chat_status import dev_plus
+from KaguyaRobot import dispatcher
+from KaguyaRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler
@@ -18,13 +18,13 @@ from telegram.ext import CallbackContext, CommandHandler
 def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
-        state = "Lockdown is " + "on" if not SiestaRobot.ALLOW_CHATS else "off"
+        state = "Lockdown is " + "on" if not KaguyaRobot.ALLOW_CHATS else "off"
         update.effective_message.reply_text(f"Current state: {state}")
         return
     if args[0].lower() in ["off", "no"]:
-        SiestaRobot.ALLOW_CHATS = True
+        KaguyaRobot.ALLOW_CHATS = True
     elif args[0].lower() in ["yes", "on"]:
-        SiestaRobot.ALLOW_CHATS = False
+        KaguyaRobot.ALLOW_CHATS = False
     else:
         update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
         return
