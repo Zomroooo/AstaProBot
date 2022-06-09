4eb0ab0b8c15e4bf2c010573f2b541c384855365
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 23:43:33 +0530

    Update disable.py

--- KaguyaRobot/modules/disable.py
+++ KaguyaRobot/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from SiestaRobot import dispatcher
-from SiestaRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from SiestaRobot.modules.helper_funcs.misc import is_module_loaded
+from KaguyaRobot import dispatcher
+from KaguyaRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from KaguyaRobot.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
@@ -14,7 +14,7 @@ from telegram.ext import (
     RegexHandler,
 )
 from telegram.utils.helpers import escape_markdown
-from SiestaRobot.modules.language import gs
+from KaguyaRobot.modules.language import gs
 
 CMD_STARTERS = tuple(CMD_STARTERS)
 
@@ -23,12 +23,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from SiestaRobot.modules.helper_funcs.chat_status import (
+    from KaguyaRobot.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from SiestaRobot.modules.sql import disable_sql as sql
+    from KaguyaRobot.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
