ef942afd64e5e0092c6d96afed291c8406400d83
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 01:06:43 +0530

    Update chat_status.py

--- KaguyaRobot/modules/helper_funcs/chat_status.py
+++ KaguyaRobot/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from SiestaRobot import (
+from KaguyaRobot import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from SiestaRobot.modules import connection
+from KaguyaRobot.modules import connection
 
 connected = connection.connected
