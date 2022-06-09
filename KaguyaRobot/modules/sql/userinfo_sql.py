8ca504829035976f44d56617fe46f224babe62b7
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 01:01:48 +0530

    Update userinfo_sql.py

--- KaguyaRobot/modules/sql/userinfo_sql.py
+++ KaguyaRobot/modules/sql/userinfo_sql.py
@@ -1,5 +1,5 @@
 import threading
-from SiestaRobot.modules.sql import BASE, SESSION
+from KaguyaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, UnicodeText
 from sqlalchemy.sql.sqltypes import BigInteger
 
