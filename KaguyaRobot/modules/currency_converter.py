c2ef9745c848b31d8083cec5d67060e2f4c70754
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 23:47:00 +0530

    Update currency_converter.py

--- KaguyaRobot/modules/currency_converter.py
+++ KaguyaRobot/modules/currency_converter.py
@@ -1,6 +1,6 @@
 import requests
 
-from SiestaRobot import CASH_API_KEY, dispatcher
+from KaguyaRobot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
