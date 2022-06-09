f932f25f33bafc16133d59c169bd528028d8a831
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 01:04:10 +0530

    Update chatstatus.py

--- KaguyaRobot/modules/helper_funcs/telethn/chatstatus.py
+++ KaguyaRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from SiestaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from SiestaRobot import DRAGONS
+from KaguyaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from KaguyaRobot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
