69cde59cf461c08a915119ab904988c68c5b7cc8
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-09 23:27:41 +0530

    Update language.py

--- KaguyaRobot/modules/language.py
+++ KaguyaRobot/modules/language.py
@@ -1,5 +1,5 @@
 import itertools
-import SiestaRobot.modules.sql.language_sql as sql
+import KaguyaRobot.modules.sql.language_sql as sql
 
 from typing import Union, List, Dict, Callable, Generator, Any
 
@@ -7,9 +7,9 @@ from collections.abc import Iterable
 from telegram.ext import CommandHandler, CallbackQueryHandler
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 
-from SiestaRobot import dispatcher
-from SiestaRobot.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
-from SiestaRobot.language import get_string, get_languages, get_language
+from KaguyaRobot import dispatcher
+from KaguyaRobot.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from KaguyaRobot.language import get_string, get_languages, get_language
 
 
 
