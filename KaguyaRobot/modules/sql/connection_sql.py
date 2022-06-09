fc51ca40ac04e2a9ca66a9b9bbce3222c15271c5
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 00:56:11 +0530

    Update connection_sql.py

--- KaguyaRobot/modules/sql/connection_sql.py
+++ KaguyaRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from SiestaRobot.modules.sql import SESSION, BASE
+from KaguyaRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
