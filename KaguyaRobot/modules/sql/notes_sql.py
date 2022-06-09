5669e8f59350de7a409b1703e84e2ecb4c651e38
Babycute11 <97697747+Babycute11@users.noreply.github.com>
2022-06-10 01:00:27 +0530

    Update notes_sql.py

--- KaguyaRobot/modules/sql/notes_sql.py
+++ KaguyaRobot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from SiestaRobot.modules.helper_funcs.msg_types import Types
-from SiestaRobot.modules.sql import BASE, SESSION
+from KaguyaRobot.modules.helper_funcs.msg_types import Types
+from KaguyaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText, distinct, func, Integer
 
 
